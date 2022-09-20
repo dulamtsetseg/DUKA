9a2ab29b5f294ef020fcbb93c3920e9be9952fbe
Baasanhvv <101857516+Baasanhvv@users.noreply.github.com>
2022-09-20 11:42:07 +0800

    qwe

--- duka/src/App.js
+++ duka/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 const App = () => {
-const name = 'bye';
-const isnameShowing = true;
+const name = 'Duka';
+const someone = 'someone';
+const isnameShowing = false;
   return (
     <div className="App">
-      <h1>Hello,{isnameShowing ? name : someone}</h1>
+      <h1>Hello, {isnameShowing ? name : someone}</h1>
+      
       
     </div>
   );
