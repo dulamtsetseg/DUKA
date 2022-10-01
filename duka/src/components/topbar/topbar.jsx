133658238db0b3f8f17f650ff1edbed7741febf3
dulamtsetseg <112919649+dulamtsetseg@users.noreply.github.com>
2022-10-01 00:01:19 -0700

    4

--- duka/src/components/topbar/topbar.jsx
+++ duka/src/components/topbar/topbar.jsx
@@ -1,5 +1,5 @@
 import "./topbar.css"
-
+import { Search,Person, Chat, Notifications} from "@material-ui/icons"
 export default function Topbar() {
   return (
     <div className="topbarContainer">
@@ -7,12 +7,31 @@ export default function Topbar() {
         <span className="logo">Lamasocial</span>
       </div>
       <div className="topbarCenter"></div>
-       <div className="searchbar"></div>
-       
-
-      <div className="topbarRight"></div>
-
-
+      <div className="searchbar">
+        <Search /> 
+        <input placeholder="Search for friend, post or video " className="searchInput" />
+       </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topLinks">Homepage</span>
+          <span className="topLinks">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarItemIcons">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarItemIcons">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarItemIcons">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/1.jpeg" alt="" className="topbarImg" />
       </div>
-  ) 
+      </div>
+  )     
 }
