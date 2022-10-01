eb31c85328a6a1681f8c731d2b32330bbf731433
dulamtsetseg <112919649+dulamtsetseg@users.noreply.github.com>
2022-09-30 18:27:31 -0700

    3

--- duka/src/components/topbar/topbar.js
+++
@@ -1,8 +0,0 @@
-import "./topbar.css"
-
-export default function topbar() {
-  return (
-    <div className="topbar">
-    </div>
-  )
-}
---
+++ duka/src/components/topbar/topbar.jsx
@@ -0,0 +1,18 @@
+import "./topbar.css"
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter"></div>
+       <div className="searchbar"></div>
+       
+
+      <div className="topbarRight"></div>
+
+
+      </div>
+  ) 
+}
