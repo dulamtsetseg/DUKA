ed91938322bcda81ce44e5d56bc762e80dc22f48
dulamtsetseg <112919649+dulamtsetseg@users.noreply.github.com>
2022-10-07 21:27:25 -0700

    2

--- duk2/onokagana/src/App.js
+++ duk2/onokagana/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Search  from './components/search/search';
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
   return (
-    <div className="App">
+    <div className=" ">
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
-
+  
 export default App;
