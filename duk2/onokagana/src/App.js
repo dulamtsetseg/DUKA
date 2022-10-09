118fb1a87bbaeacc090bdba2016f90815aaa93fa
dulamtsetseg <112919649+dulamtsetseg@users.noreply.github.com>
2022-10-08 23:24:55 -0700

    6

--- duk2/onokagana/src/App.js
+++ duk2/onokagana/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Search  from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
-
+import {WEATHER_API_URL,  WEATHER_API_KEY} from './api';
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+   const [lat, lon] = searchData.value.split(" ");
+
+   const CurrentWeather = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+   
+   Promise.all([currentWeatherFech, forecasFetch])
+     .then(async (response) => {
+      const weatherResponse =  await response[0].json();
+  })
+  
   return (
     <div className="Container">
       <Search onSearchChange={handleOnSearchChange}/>
--- duk2/onokagana/src/api.js
+++ duk2/onokagana/src/api.js
@@ -7,5 +7,7 @@ export const geoApiOptions = {
 };
 
     
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8aa422c4cef598760adc0b73e19eeb6e";
