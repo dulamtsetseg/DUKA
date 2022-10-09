5cd686586b175a7143429d92e9480844c911e6ad
dulamtsetseg <112919649+dulamtsetseg@users.noreply.github.com>
2022-10-09 00:30:11 -0700

    7

--- duk2/onokagana/src/App.js
+++ duk2/onokagana/src/App.js
@@ -2,25 +2,33 @@ import './App.css';
 import Search  from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import {WEATHER_API_URL,  WEATHER_API_KEY} from './api';
-function App() {
+import { useState } from 'react';
 
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
   const handleOnSearchChange = (searchData) => {
    const [lat, lon] = searchData.value.split(" ");
 
-   const CurrentWeather = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
    
-   Promise.all([currentWeatherFech, forecasFetch])
+   Promise.all([currentWeatherFetch, forecastFetch])
      .then(async (response) => {
       const weatherResponse =  await response[0].json();
+      const forecastResponse =  await response[1].json();
+
+      setCurrentWeather({city: searchData.label, ...weatherResponse});
+      setForecast({city: searchData.label, ...forecastResponse});
   })
   
   return (
     <div className="Container">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
     </div>
   );
-}
+};
+};
 
 export default App;
--- duk2/onokagana/src/components/current-weather/current-weather.js
+++ duk2/onokagana/src/components/current-weather/current-weather.js
@@ -1,13 +1,13 @@
     import "./current-weather.css"
 
-    const CurrentWeather = () => {
+    const CurrentWeather = ({data}) => {
         return (
             <div className="weather">
                 <div className="top">
                     <div>
 
                     
-                    <p className="city">Belgrade</p>
+                    <p className="city">{data.city}</p>
                     <p className="weather-description">Sunny</p>
                     </div>
                 <img alt="weather" className="weather-icon" src="zurag/1.jpeg" />
