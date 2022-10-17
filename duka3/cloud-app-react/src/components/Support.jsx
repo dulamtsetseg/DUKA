64ffc4c7a5917d2890ba251e21fa2e674c147446
dulamtsetseg <>
2022-10-17 08:28:14 -0700

    10

--- duka3/cloud-app-react/src/components/Support.jsx
+++ duka3/cloud-app-react/src/components/Support.jsx
@@ -3,13 +3,35 @@ import {PhoneIcon, ArrowSmRightIcon} from '@heroicons/react/outline'
 import {ChipIcon, SupportIcon} from '@heroicons/react/solid'
 
 
+import supportImg from '../assets/jpg'
 function Support() {
   return (
-    <div>
-        <dov>
-            Hello
-        </dov>
+  <div className='w-full h-screen mt-24'>
+        <div className='w-full h-[700px bg-gray-900/90 absolute'>
+            <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
+        </div>
+        <div>
+            <div>
+                <h2>Support</h2>
+                <h3>愛して殺す</h3>
+                <p>大嫌い</p>
+            </div>\
+             
+            <div>
+                <div>
+                    <div>
+                        <PhoneIcon />
+                        <h3>愛</h3>
+                        <p>yu chmaq bic gseeee</p>
+                    </div>
+                    <div>
+                        <p>Contant us <ArrowSmRightIcon/> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
   )
 }
 
