5292a8e79d2a8f0804c9a09f19ef333eace157b2
dulamtsetseg <>
2022-10-19 09:35:57 -0700

    essa

--- duka3/cloud-app-react/src/App.js
+++ duka3/cloud-app-react/src/App.js
@@ -2,6 +2,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import About from './components/About';
 import Support from './components/Support';
+import AllInOne from './components/AllInOne';
+import Pricing from './components/Pricing';
 
 function App() {
    return (
@@ -10,6 +12,8 @@ function App() {
       <Hero />
       <About/>
       <Support/>
+      <AllInOne/>
+      <Pricing />
     </>
   );
 }
--- duka3/cloud-app-react/src/components/Support.jsx
+++ duka3/cloud-app-react/src/components/Support.jsx
@@ -1,49 +1,49 @@
 import React from 'react'
 import {PhoneIcon, ArrowSmRightIcon} from '@heroicons/react/outline'
 import {ChipIcon, SupportIcon} from '@heroicons/react/solid'
-import supportImg from '../assets/3.png'
+import supportImg from '../assets/team.jpg'
 
 function Support() {
   return (
-  <div className='w-full h-screen mt-24'>
-        <div className='w-full h-[700px bg-gray-900/90 absolute'>
+  <div className='w-full mt-24'>
+        <div className='w-full h-[700px] bg-gray-900/50 absolute'>
             <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
         </div>
         <div className='max-w-[1240px] mx-auto text-white relative'>
             <div className='px-4 py-12'>
-                <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>見てください</h2>
-                <h3 className='text-5xl font-bold py-6 text-center'>幸せと愛はお金</h3>
-                <p className='py-4 text-3xl text-slate-300'>幸福、お金、愛の3つのうちどれを選びますか?</p>
+                <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
+                <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team </h3>
+                <p className='py-4 text-3xl text-slate-300'>Lorem ipsum dolor sit amet consettetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minute ea quidem quisquam unde beatae, minus illo et cum vel?</p>
             </div>
              
             <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
                 <div className='bg-white rounded-xl shadow-2xl'>
-                    <div className='p-8  max-h-80 h-80'>
+                    <div className='p-8'>
                         <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
-                        <h3 className='font-bold text-2xl my-6'>幸福</h3>
-                        <p className='text-gray-600 text-xl'>a幸福とは、健康で、幸せで、平和で、人生に満足しているという長期的な感覚です。これは、私たちが常に求めているものです。私たちはそれを少し味わったとき、それが永遠に続くことを望んでいます.</p>
+                        <h3 className='font-bold text-2xl my-6'>Sales</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
                     </div>
-                    <div className='bg-slate-100 pl-8 py-4 mt-7'>   
-                        <p className='flex items-center text-indigo-600'>Contact us <ArrowSmRightIcon className='w-5 ml-2'/> </p>
-                    </div>
-                </div>
-                <div className='bg-white rounded-xl shadow-2xl'>
-                    <div className='p-8   max-h-80 h-80'>
-                        <SupportIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
-                        <h3 className='font-bold text-2xl my-6'>お金</h3>
-                        <p className='text-gray-600 text-xl '>が高く、金に次ぐものです。すべての金属よりも電熱をよく伝導します。</p>
-                    </div>
-                    <div className='bg-slate-100 pl-8 py-4 mt-7' >   
+                    <div className='bg-slate-100 pl-8 py-4'>   
                         <p className='flex items-center text-indigo-600'>Contant us <ArrowSmRightIcon className='w-5 ml-2'/> </p>
                     </div>
                 </div>
                 <div className='bg-white rounded-xl shadow-2xl'>
-                    <div className='p-8 max-h-80 h-80'>
-                        <ChipIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
-                        <h3 className='font-bold text-2xl my-6'>お金</h3>
-                        <p className='text-gray-600 text-xl'>私たちの惑星では、純粋に個別に、または組み合わせて発生します。世界では、この金属は金の20倍、銅のはるかに少ないです。純銀は光沢のある白い金属で、非常に柔らかく再現性が高く、金に次ぐものです。すべての金属よりも電熱をよく伝導します</p>
+                    <div className='p-8'>
+                        <SupportIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+                        <h3 className='font-bold text-2xl my-6'>Technical Support</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsut dolor sit amet consectetur adipisicing alit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
                     </div>
-                    <div className='bg-slate-100 pl-8 py-4  mt-7'>
+                    <div className='bg-slate-100 pl-8 py-4'>   
+                        <p className='flex items-center text-indigo-600'>Contant us <ArrowSmRightIcon className='w-5 ml-2'/> </p>
+                    </div>
+                </div>
+                <div className='bg-white rounded-xl shadow-2xl'>
+                    <div className='p-8'>
+                        <ChipIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+                        <h3 className='font-bold text-2xl my-6'>Media Inquiries</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consettetur adipisicing elit, Excepturi provident iure placeat blanditiisea sint earum hic iste quibusdam exercitationem.</p>
+                    </div>
+                    <div className='bg-slate-100 pl-8 py-4'>
                         <p className='flex items-center text-indigo-600'>Contact us<ArrowSmRightIcon className='w-5 ml-2'/></p>
                     </div>
                 </div>
