7b3ca9b326e7c0e68b78a1a4b4170c74277ae1cd
dulamtsetseg <>
2022-10-20 03:31:05 -0700

    recipe

---
+++ duka5/react-recipe/src/App.js
@@ -0,0 +1,16 @@
+import Category from "./components/Category";
+import Pages from "./pages/Pages";
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Category/>
+        <Pages/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
